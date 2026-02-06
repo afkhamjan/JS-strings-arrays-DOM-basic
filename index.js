16dfb2ce29f1cc17303022f35bba1e84509f965d
Afkham Jan <afkhamjan7@gmail.com>
2026-02-07 01:54:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("this is strings tutorial")
+let a = "harry";
+console.log(a)
+console.log(a[0])
+
+console.log(a.length)
+
+let name ="afkham"
+let friend = "harry"
+console.log("his name is "+ name + " and his friends name is " + friend)
+
+
+console.log(`his name is ${name } and his friends name is ${friend}`)
+
+let b= "ahmad"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+
+console.log(b.slice(1,4))
+console.log(b.slice(2))
+
+console.log(b.concat(a))
---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("hello world")
+document.body.childNodes[1].firstElementChild
+document.body.style.backgroundColor ="grey" 
+document.body.firstElementChild.children[0].nextElementSibling
+document.body.firstElementChild.children[3].previousElementSibling
